4267ceca48e7eb77cdf0a0d8bbd062efb3190fcd
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2020-02-03 15:02:27 +0000

    [SbGame] Let each actor have its own inventory (to let creatures and characters (NPCs + player(s)) to also have it)

--- src/SbGame/Actor.hpp
+++ src/SbGame/Actor.hpp
@@ -3,7 +3,19 @@
 #pragma once
 
 #include "Entity.hpp"
+#include "Inventory.hpp"
 
 class CActor : public CEntity
 {
+	bool GiveInventoryItem(SbDict *apItem, uint32_t anFlags);
+	void RemoveInventoryItem(SbDict *apItem);
+	
+	void UseEquippedItem();
+	
+	Item &GetEquippedItem() const {return mInventory.GetEquippedItem();}
+	Item &GetEquippedBodyWear() const {return mInventory.GetEquippedBodyWear();}
+	Item &GetEquippedHeadWear() const {return mInventory.GetEquippedHeadWear();}
+	Item &GetEquippedHandWear() const {return mInventory.GetEquippedHandWear();}
+private:
+	Inventory mInventory;
 };
