2394836306fbbb3b2db3d13644954e12189b9473
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2020-02-21 14:54:29 +0000

    Move all the game logic code here from the engine repo (2)

---
+++ src/SbGame/mwworld/nullaction.hpp
@@ -0,0 +1,17 @@
+#ifndef GAME_MWWORLD_nullptrACTION_H
+#define GAME_MWWORLD_nullptrACTION_H
+
+#include "action.hpp"
+
+namespace MWWorld
+{
+    /// \brief Action: do nothing
+    class NullAction : public Action
+    {
+            virtual void executeImp (const Ptr& actor) {}
+
+            virtual bool isNullAction() { return true; }
+    };
+}
+
+#endif
